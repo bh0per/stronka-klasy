8167ff5b7b011d761b43aeca8cc59b98b230bacf
werbel <78509714+bh0per@users.noreply.github.com>
2025-02-24 14:22:21 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const themeToggleButton = document.getElementById("theme-toggle");
+const themeIcon = document.getElementById("theme-icon");
+const toggleTextButtons = document.querySelectorAll(".toggle-text");
+const resetContentButton = document.getElementById("reset-content");
+const newContents = document.querySelectorAll(".new-content");
+
+themeToggleButton.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    if (document.body.classList.contains("dark-mode")) {
+        themeIcon.src = "https://cdn-icons-png.flaticon.com/512/10370/10370098.png"; 
+    } else {
+        themeIcon.src = "https://cdn-icons-png.flaticon.com/512/10370/10370228.png"; 
+    }
+});
+
+toggleTextButtons.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        newContents.forEach(content => content.style.display = "none");
+        newContents[index].style.display = "block";
+    });
+});
+
+resetContentButton.addEventListener("click", () => {
+    newContents.forEach(content => content.style.display = "none");
+});
