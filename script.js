efaf4e2a202f321fc83129998a8d95996ccc161f
werbel <78509714+bh0per@users.noreply.github.com>
2025-02-27 18:57:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,15 @@ resetContentButton.addEventListener("click", () => {
     newContents.forEach(content => content.style.display = "none");
 });
 
+(function() {
+    window.chtlConfig = { chatbotId: "5548387222" };
+
+    var script = document.createElement("script");
+    script.async = true;
+    script.dataset.id = "5548387222";
+    script.id = "chatling-embed-script";
+    script.type = "text/javascript";
+    script.src = "https://chatling.ai/js/embed.js";
+
+    document.head.appendChild(script);
+})();
