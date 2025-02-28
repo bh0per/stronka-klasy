8d13f03f10bd19181228f76208b5d8d5016e26fd
werbel <78509714+bh0per@users.noreply.github.com>
2025-02-28 19:00:09 +0100

    Update script.js

--- script.js
+++ script.js
@@ -37,15 +37,3 @@ resetContentButton.addEventListener("click", () => {
 
     document.head.appendChild(script);
 })();
-function initMap(){
-    var location = {lat:52.248264, lng:21.009919 };
-    var map = new google.maps.Map(document.getElementById("map"), {
-        zoom:14,
-        center:location
-    });
-    var marker = new google.maps.Marker({
-        position:location,
-        map:map
-    });
-}
-
